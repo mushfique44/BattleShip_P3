07057614f763736c4e3fcf7b546d22846b8c18db
mushfique44 <mushfique44@gmail.com>
2024-07-24 19:57:49 +0000

    creating welcome page and board size pick

--- run.py
+++ run.py
@@ -13,7 +13,35 @@ GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('Battleship_P3')
 
 frBYfr = SHEET.worksheet('4BY4')
+comp_frBYfr = SHEET.worksheet('comp4BY4')
+fiveBYfive = SHEET.worksheet('5BY5')
+comp_fiveBYfive = SHEET.worksheet('comp5BY5')
 
-data = frBYfr.get_all_values()
+GAME_ONE = frBYfr.get_all_values()
+GAME_TWO = fiveBYfive.get_all_values()
 
-print(data)
+
+
+def welcome_page():
+
+    print("Welcome! Please chose board size by entering:")
+    print("1: 4by4 grid")
+    print("2: 5by5 grid")
+
+    data_int = input("Chose between 1 or 2: ")
+    if data_int == "1":
+        print(frBYfr.row_values(1))
+        print(frBYfr.row_values(2))
+        print(frBYfr.row_values(3))
+        print(frBYfr.row_values(4))                        
+    else:
+        print(fiveBYfive.row_values(1))
+        print(fiveBYfive.row_values(2))
+        print(fiveBYfive.row_values(3))
+        print(fiveBYfive.row_values(4))
+        print(fiveBYfive.row_values(5))
+
+welcome_page()
+
+def comp_board_picks():
+    print("")
